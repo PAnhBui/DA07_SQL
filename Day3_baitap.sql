89ba60d1a994b7aae667af77e9f92515bb3cc0ed
PAnhBui <165234917+PAnhBui@users.noreply.github.com>
2024-03-28 16:44:42 +0700

    Update Day3_baitap.sql

--- Day3_baitap.sql
+++ Day3_baitap.sql
@@ -3,7 +3,7 @@ select name from city where population>120000 and countrycode = 'USA';
 --ex2
 select * from city where countrycode = 'JPN';
 --ex3
-
+SELECT city, states from station;
 --ex4
 
 --ex5
