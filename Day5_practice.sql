326318d45ab790269a712b6175e0e119eb2b20b1
PAnhBui <165234917+PAnhBui@users.noreply.github.com>
2024-04-03 00:19:58 +0700

    Update Day5_practice.sql

--- Day5_practice.sql
+++ Day5_practice.sql
@@ -9,7 +9,6 @@ SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
 3. đk lọc theo trường nào (gốc hay phái sinh)*/
 SELECT ROUND(CAST(SUM(item_count*order_occurrences) / SUM(order_occurrences)as decimal),1) as mean
 FROM items_per_order;
-
 --ex5
 /* ptich yêu cầu
 1. output (gốc/ phái sinh) candidate_id
@@ -31,14 +30,12 @@ FROM posts
 WHERE post_date>='2021-01-01' and post_date<='2022-01-01'
 group by user_id
 having COUNT(post_id)>=2
-
 --ex7
 select card_name,
 max(issued_amount)-min(issued_amount) as difference
 from monthly_cards_issued
 group by card_name
 order by difference DESC
-
 --ex8
 /* ptich yêu cầu
 1. output (gốc/ phái sinh) manufacturer,drug_count,total_loss
@@ -51,6 +48,28 @@ FROM pharmacy_sales
 where total_sales< cogs
 group by manufacturer
 order by total_loss desc
-
 --ex9
+/* phân tích yêu cầu
+1. output (gốc/ phái sinh) id | movie      | description | rating
+2. input
+3. đk lọc theo trường nào (gốc hay phái sinh):odd ID,descrip not borring rating desc*/ 
+select * from cinema
+where id%2=1 and description<>'boring'
+order by rating desc
+--ex10
+select teacher_id,
+count (distinct subject_id) as cnt
+from teacher
+group by teacher_id
+--ex11
+select user_ID,
+count (follower_id) as followers_count
+from followers
+group by user_id
+order by user_id
+--ex12
+select class from courses
+group by class
+having count (student) >=5
+
 
