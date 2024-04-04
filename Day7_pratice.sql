7c18d40e4a0d901a5aedf9fc0db6f5bd8b2aefa2
PAnhBui <165234917+PAnhBui@users.noreply.github.com>
2024-04-04 20:35:16 +0700

    Update Day7_pratice.sql

--- Day7_pratice.sql
+++ Day7_pratice.sql
@@ -16,3 +16,50 @@ SELECT manufacturer,
 FROM pharmacy_sales
 GROUP BY manufacturer
 order by SUM(total_sales) desc
+--ex4
+SELECT 
+extract(month from submit_date) as mth,
+product_id,
+round (avg (stars),2) as avg_stars
+FROM reviews
+GROUP BY mth, product_id
+ORDER BY mth, product_id
+--ex5
+SELECT 
+sender_id,
+count (message_id) as message_count
+FROM messages
+WHERE extract(month from 	sent_date) = 8
+and extract(year from sent_date) = 2022
+group by sender_id
+order by message_count desc
+limit 2
+--ex6
+select
+tweet_id
+from Tweets
+where length (content) >15
+--ex7
+select
+activity_date as day,
+count (distinct  user_id) as active_users 
+from Activity
+WHERE (activity_date > '2019-06-27' AND activity_date <= '2019-07-27')
+group by day
+--ex8
+select 
+count (id) as number_of_employees
+from employees
+where extract (month from joining_date) between 1 and 7 and extract (year from joining_date) =2022
+--ex9
+select 
+position ('a' in First_name)
+from worker
+where first_name = 'Amitah' 
+--ex10
+
+select 
+substring (title,length(winery) +2,4 ) as the_year
+from winemag_p2
+where country ='Macedonia'
+
