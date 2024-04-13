a58f48d9083655ea243ccc58110d5f623bac095b
PAnhBui <165234917+PAnhBui@users.noreply.github.com>
2024-04-13 18:21:36 +0700

    Update Day13_practice_problemsolvingconcept.sql

--- Day13_practice_problemsolvingconcept.sql
+++ Day13_practice_problemsolvingconcept.sql
@@ -35,3 +35,67 @@ FROM spending_cte
 WHERE ranking <= 2 
 ORDER BY category, ranking;
 
+--ex3
+WITH call_records AS (
+SELECT
+  policy_holder_id,
+  COUNT(case_id) AS call_count
+FROM callers
+GROUP BY policy_holder_id
+HAVING COUNT(case_id) >= 3
+)
+SELECT COUNT(policy_holder_id) AS member_count
+FROM call_records;
+
+--ex4
+SELECT 
+a.page_id
+FROM pages as a
+LEFT JOIN page_likes as b
+ON a.page_id=b.page_id
+WHERE liked_date is NULL
+ORDER BY a.page_id
+--ex5
+ELECT 
+  EXTRACT(MONTH FROM curr_month.event_date) AS month, 
+  COUNT(DISTINCT curr_month.user_id) AS monthly_active_users 
+FROM user_actions AS curr_month
+WHERE EXISTS (
+  SELECT last_month.user_id 
+  FROM user_actions AS last_month
+  WHERE last_month.user_id = curr_month.user_id
+    AND EXTRACT(MONTH FROM last_month.event_date) =
+    EXTRACT(MONTH FROM curr_month.event_date - interval '1 month')
+)
+  AND EXTRACT(MONTH FROM curr_month.event_date) = 7
+  AND EXTRACT(YEAR FROM curr_month.event_date) = 2022
+GROUP BY EXTRACT(MONTH FROM curr_month.event_date);
+
+--ex6
+SELECT 
+EXTRACT (YEAR from  trans_date)||' - '|| EXTRACT (month from  trans_date) as month,
+country,
+COUNT (State) as trans_count,
+SUM (CASE WHEN state ='approved' then 1 else 0 end) as approved_count, 
+SUM (amount) as trans_total_amount,
+SUM (CASE WHEN state ='approved' then amount else 0 end) as approved_total_amount
+FROM Transactions
+GROUP BY month, country
+ORDER BY month
+
+--ex7
+SELECT product_id, year AS first_year, quantity, price
+FROM Sales
+WHERE (product_id, year) in 
+    (SELECT product_id, MIN(year) 
+    FROM Sales
+    GROUP BY product_id)
+
+--ex8
+SELECT customer_id 
+FROM Customer 
+GROUP BY customer_id 
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(product_key) FROM Product);
+
+--EX9
+
